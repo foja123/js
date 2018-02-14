03c2fb9e34e6982eeb843c7174d8f21eef8b1313
Andrea Serafin <andrea.serafin@qmi.it>
2018-02-14 18:26:39 +0100

    executione context

--- scripts/app.js
+++ scripts/app.js
@@ -1,47 +1,26 @@
-function App (a,b) {
-
-    this.elementToMove = $('.'+a);
-    this.btnThatMove = $('#'+b);
-
-    this.events();
-
+function parseURL(url) {
+    var parser = document.createElement('a'),
+        searchObject = {},
+        queries, split, i;
+    // Let the browser do the work
+    parser.href = url;
+    // Convert query string to object
+    queries = parser.search.replace(/^\?/, '').split('&');
+    for( i = 0; i < queries.length; i++ ) {
+        split = queries[i].split('=');
+        searchObject[split[0]] = split[1];
+    }
+    return {
+        protocol: parser.protocol,
+        host: parser.host,
+        hostname: parser.hostname,
+        port: parser.port,
+        pathname: parser.pathname,
+        search: parser.search,
+        searchObject: searchObject,
+        hash: parser.hash
+    };
 }
 
 
-App.prototype.getElementTomove = function() {
-
-    return this.elementToMove;
-
-}
-
-App.prototype.moveElement = function() {
-
-    this.elementToMove.animate({
-        marginLeft: '+=' + '100px'
-    },1000)
-}
-
-
-
-App.prototype.events = function () {
-    console.log("events");
-    //this.btnThatMove.click(this.moveElement.bind(this));
-
-}
-
-
-
-$(document).ready(function() {
-
-
-    var app = new App('block1','move1');
-
-    var bapp = new App('block2','move2');
-
-    
-    console.log(app.getElementTomove());
-
-    console.log(bapp.getElementTomove());
-
-
-});
+var a = null;
